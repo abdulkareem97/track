357796552d026cde62e61e6339aa6873163b460f
Abdul Kareem <rajab8321@gmail.com>
2023-09-23 01:45:52 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
               longitude
             }
             // const data = await axios.post('http://192.168.0.198:1000/post',d)
-            const data = await axios.post('https://ecb8-2401-4900-62a5-49d4-d94b-7664-c997-ad21.ngrok-free.app:1000/post',d)
+            const data = await axios.post('http://192.168.0.198:1000/post',d)
             console.log(data)
             
-            console.log(latitude, longitude);
+            // console.log(latitude, longitude);
           },
           (error) => {
             // Handle geolocation error
